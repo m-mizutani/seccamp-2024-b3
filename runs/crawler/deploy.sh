f74d5216fd7a23e5175c245074d7c2aa616e17a1
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2024-07-20 10:13:28 +0900

    Update Docker build command to specify platform

--- runs/crawler/deploy.sh
+++ runs/crawler/deploy.sh
@@ -3,6 +3,6 @@
 PROJECT="mztn-seccamp-2024"
 
 gcloud auth configure-docker asia-northeast1-docker.pkg.dev
-docker build -t crawler .
+docker build -t crawler --platform linux/amd64 .
 docker tag crawler asia-northeast1-docker.pkg.dev/$PROJECT/containers-$ID/crawler
 docker push asia-northeast1-docker.pkg.dev/$PROJECT/containers-$ID/crawler
--- runs/detector/deploy.sh
+++ runs/detector/deploy.sh
@@ -3,6 +3,6 @@
 PROJECT="mztn-seccamp-2024"
 
 gcloud auth configure-docker asia-northeast1-docker.pkg.dev
-docker build -t detector .
+docker build -t detector --platform linux/amd64 .
 docker tag detector asia-northeast1-docker.pkg.dev/$PROJECT/containers-$ID/detector
 docker push asia-northeast1-docker.pkg.dev/$PROJECT/containers-$ID/detector
