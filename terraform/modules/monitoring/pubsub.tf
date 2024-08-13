9ed468b0c5fea1bbfcd8af9f47a15890df5a1306
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2024-08-13 13:34:03 +0900

    Add new Pub/Sub resources for testing

--- terraform/modules/monitoring/pubsub.tf
+++ terraform/modules/monitoring/pubsub.tf
@@ -17,14 +17,37 @@ resource "google_pubsub_subscription" "notify" {
   }
 }
 
-resource "google_pubsub_topic_iam_member" "notify" {
+resource "google_pubsub_topic_iam_member" "detector_is_publisher" {
   topic  = google_pubsub_topic.notify.name
   role   = "roles/pubsub.publisher"
   member = "serviceAccount:${google_service_account.detector.email}"
 }
 
-resource "google_pubsub_topic_iam_member" "owner_is_viewer" {
+resource "google_pubsub_topic_iam_member" "owner_is_admin" {
   topic  = google_pubsub_topic.notify.name
+  role   = "roles/pubsub.admin"
+  member = "user:${var.owner}"
+}
+
+resource "google_pubsub_topic" "test" {
+  project = var.project_id
+  name    = "test-${var.id}"
+}
+
+resource "google_pubsub_subscription" "test" {
+  project              = var.project_id
+  name                 = "test-${var.id}"
+  topic                = google_pubsub_topic.test.name
+}
+
+resource "google_pubsub_topic_iam_member" "detector_is_test_publisher" {
+  topic  = google_pubsub_topic.test.name
+  role   = "roles/pubsub.publisher"
+  member = "serviceAccount:${google_service_account.detector.email}"
+}
+
+resource "google_pubsub_topic_iam_member" "owner_is_test_admin" {
+  topic  = google_pubsub_topic.test.name
   role   = "roles/pubsub.admin"
   member = "user:${var.owner}"
 }
